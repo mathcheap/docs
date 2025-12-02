ee150893d1bbeb5929c8f9383144f1b2c14ecf4d
shenlu89 <shenlu89dev@gmail.com>
2025-12-02 11:15:31 +0800

    refactor: consolidate social icons into array and update links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,26 @@
 "use client";
 
 import { type HTMLAttributes } from "react";
+import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-
-import { DiscordButton } from "@/components/discord-button";
-import { GithubButton } from "@/components/github-button";
+import { FaDiscord, FaWeixin, FaGithub } from "react-icons/fa6";
+import Link from "@/components/custom-link";
 import ThemeButton from "@/components/theme-button";
+
+const SocialIcons = [
+  {
+    title: "Discord",
+    href: "https://discord.gg/Q4chBVDFvK",
+    icon: <FaDiscord className="size-6" />,
+  },
+  {
+    title: "GitHub",
+    href: "https://github.com/mathcheap/docs",
+    icon: (
+      <FaGithub className="size-6" />
+    ),
+  },
+];
 
 interface Navbar extends HTMLAttributes<HTMLDivElement> { }
 
@@ -14,11 +29,12 @@ export const AppNavbar: React.FC<Navbar> = ({
   children,
   ...props
 }) => {
+
   return (
     <header
       className={cn(
         "w-full justify-end mx-auto h-16 px-8 lg:px-8 fixed top-0 items-center md:flex hidden",
-        "bg-background dark:bg-background",
+        "bg-background dark:bg-black",
         "border-b border-border",
         className,
       )}
@@ -26,8 +42,18 @@ export const AppNavbar: React.FC<Navbar> = ({
     >
       <nav className="flex items-center gap-4">
         <ThemeButton />
-        <DiscordButton />
-        <GithubButton />
+        <ul className="flex justify-center items-center space-x-4">
+          {SocialIcons.map((social) => (
+            <Link
+              className="text-slate-600 dark:text-slate-300 hover:text-black dark:hover:text-slate-200"
+              title={social.title}
+              key={social.title}
+              href={social.href}
+            >
+              {social.icon}
+            </Link>
+          ))}
+        </ul>
       </nav>
     </header>
   );
