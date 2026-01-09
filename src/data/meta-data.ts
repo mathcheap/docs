7939919db6ac0a39cd67655192c09c5f0b2d7b11
shenlu89 <shenlu89dev@gmail.com>
2026-01-09 12:06:05 +0800

    chore: update version to 0.11.5 and add changelog entries

--- src/data/meta-data.ts
+++ src/data/meta-data.ts
@@ -50,7 +50,7 @@ export const DocsPage = {
     },
   },
   title: "Mathcheap",
-  version: "0.11.4",
+  version: "0.11.5",
   description:
     "Mathcheap Documentation",
   url: "https://mathcheap.xyz/docs",
